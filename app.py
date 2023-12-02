8f9cade6b803d9bf4e1b3c490dd26f720361957d
Elliott Arnold <si3mshady@Elliotts-MBP.lan>
2023-12-01 20:26:20 -0600

    save

--- app.py
+++ app.py
@@ -13,15 +13,19 @@ class GetNutritionDetails(BaseToolSpec):
         self.data = {"query": None}
         self.USDA_URL =  "https://api.nal.usda.gov/fdc/v1/foods/search?api_key={API_KEY}"
        
-
-        
+       
     spec_functions = ["resolve_food_item"]
     
     def resolve_food_item(self,food):
+        '''
+         Given a food item this function will query the USDA API database for the same and return a document that 
+         contains ingredients, serving size and food nutritients 
+         args:
+           food: (string: the food item to query) 
+        '''
         self.data["query"] = food
 
         response = requests.post(url=self.USDA_URL.format(API_KEY=USDA_API_KEY), headers=self.headers,  data=json.dumps(self.data))
-        # print(response.json())
         res=response.json()
 
         food_dictionary = {
